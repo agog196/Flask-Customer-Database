7ae8cc8097a90295912f62590a09833b47be7aa3
Anish Gogineni <155218596+agog196@users.noreply.github.com>
2024-01-15 15:29:46 -0600

    Add files via upload
    This is the code that uses FlaskForm from flask_wtf and imports various other packages from wtforms and wtforms.validators to create a new class which is used to create a form for adding new customer details to the database. The initial starter code files do not establish a connection with a database yet, but the connection with a database will be established once form validators flash error messages and everything else works without any errors.

---
+++ forms.py
@@ -0,0 +1,15 @@
+from flask_wtf import FlaskForm
+from wtforms import StringField, IntegerField, SubmitField
+from wtforms.validators import DataRequired, Length, Email, NumberRange
+
+
+class NewCustomerForm(FlaskForm):
+    id = IntegerField('Customer ID', validators=[DataRequired(), NumberRange(1, 9999, message="Not a valid ID")])
+    name = StringField('Name', validators=[DataRequired()])
+    email = StringField('Email', validators=[DataRequired(), Email(message="Not a valid Email")])
+    phone = IntegerField('Phone Number',
+                         validators=[DataRequired(), NumberRange(1000000000, 9999999999, message="Not a valid phone "
+                                                                                                 "number")])
+    address = StringField('Address', validators=[DataRequired(), Length(1, 30, message="Character length should be "
+                                                                                       "between 1 and 30")])
+    submit = SubmitField('Add Customer')
