8ae7d488227b0301b4595e460fec140c40ee7868
Anish Gogineni <155218596+agog196@users.noreply.github.com>
2024-01-16 15:15:11 -0600

    Updated forms.py using EmailField
    This commit uses EmailField from wtforms for the email field of the new customer details form rather than StringField.

--- forms.py
+++ forms.py
@@ -1,12 +1,12 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, IntegerField, SubmitField
+from wtforms import StringField, IntegerField, SubmitField, EmailField
 from wtforms.validators import DataRequired, Length, Email, NumberRange
 
 
 class NewCustomerForm(FlaskForm):
     id = IntegerField('Customer ID', validators=[DataRequired(), NumberRange(1, 9999, message="Not a valid ID")])
     name = StringField('Name', validators=[DataRequired()])
-    email = StringField('Email', validators=[DataRequired(), Email(message="Not a valid Email")])
+    email = EmailField('Email', validators=[DataRequired(), Email(message="Not a valid Email")])
     phone = IntegerField('Phone Number',
                          validators=[DataRequired(), NumberRange(1000000000, 9999999999, message="Not a valid phone "
                                                                                                  "number")])
